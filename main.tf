cbd0abd0ebb21213a9d0feeeb9698b7d91a3c076
dadda5 <daddaop5@gmail.com>
2026-07-04 19:01:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resources "aws_instance" "script" {
+resource "aws_instance" "script" {
  ami = "ami-0aba19e56f3eaec05"
  instance_type = var.instance_type
  }
+
+resource "aws_ s3_bucker" "scriptbuccket"{
+ bucket = var.bucket_name
+
+}
